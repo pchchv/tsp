2ac4a3fdb0d44e7834f6c89292d6523d57303bb7
pchchv <ipchchv@gmail.com>
2024-10-13 19:42:02 +0300

    Implement checkHTTP and checkPing funcs

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
+import (
+	"net/http"
+	"os/exec"
+)
+
 type Check struct {
 	Name         string `yaml:"name"`
 	Type         string `yaml:"type"`
 	Host         string `yaml:"host"`
 	Port         int    `yaml:"port"`
 	ExpectedCode int    `yaml:"expected_code"`
+}
+
+func checkHTTP(url string, expectedCode int) bool {
+	resp, err := http.Get(url)
+	if err != nil {
+		return false
+	}
+
+	_ = resp.Body.Close()
+	return resp.StatusCode == expectedCode
+}
+
+func checkPing(host string) bool {
+	cmd := exec.Command("ping", "-c", "1", "-W", "2", host)
+	return cmd.Run() == nil
 }
